c1ec9c4187d4c28661bf87031365a21e90ac6901
igor <oqdoku@yandex.ru>
2024-08-13 17:36:59 +0300

    Решение задачи 27

---
+++ typescript/27.RemoveElement/index.ts
@@ -0,0 +1,13 @@
+function removeElement(nums: number[], val: number): number {
+    let i = 0;
+    while (i < nums.length) {
+        if (nums[i] == val) {
+            nums.splice(i, 1);
+            continue;
+        }
+
+        i++;
+    }
+
+    return nums.length;
+};
